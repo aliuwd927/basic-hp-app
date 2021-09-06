6a8dbf38409b0981f296b6a7b71ade197058ef5a
Andy Liu <aliu927@gmail.com>
2021-09-05 17:39:03 -0700

    added img fldr

--- src/section.jsx
+++ src/section.jsx
@@ -2,8 +2,10 @@ function SectionPage(){
   let section = (
     <div className = "article_Container">
       <article className = "article_one">Article One</article>
-      <article className = "article_two">Artical Two</article>
-      <article className = "article_three">Artical Three</article>
+      <div className ="article_Sub_Container">
+        <article className = "article_two">Artical Two</article>
+        <article className = "article_three">Artical Three</article>
+      </div>
     </div>
   )
   return section;
