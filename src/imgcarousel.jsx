bbf83a4ff5491f0a00e51bd8a2fe71deff4b5b22
Andy Liu <aliu927@gmail.com>
2021-09-05 23:45:45 -0700

    chk values

--- src/imgcarousel.jsx
+++ src/imgcarousel.jsx
@@ -4,7 +4,20 @@ import imageThree from "./img/700x300.png"
 
 function ImgCarousel(){
   let imgArray = [imageOne, imageTwo, imageThree];
-  return <img src = {imgArray[0]} alt="placeholder" />;
+  // imgArray.map((imgElement,index) =>{
+  //   console.log(imgElement,index);
+  // })
+
+  function btnClick(event){
+    console.log(event.target.innerHTML)
+  }
+  return (
+    <div className="car_con">
+      <button onClick = {btnClick}>Previous</button>
+      <img src={imgArray[0]} alt="place holder" />
+      <button onClick ={btnClick}>Advance</button>
+    </div>
+  );
 }
 
 export default ImgCarousel;
