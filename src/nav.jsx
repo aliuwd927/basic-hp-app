c95a427d2d7d26d9259d3ec68d5548f10d8bb3e0
Andy Liu <aliu927@gmail.com>
2021-09-11 20:50:50 -0700

    updated code

--- src/nav.jsx
+++ src/nav.jsx
@@ -1,9 +1,9 @@
 function NavBar() {
   return (
-    <nav className = "navBar_container">
+    <nav className = "navBar_Container">
         <ul className ="navBar_ul">
           <li className ="navBar_li">Home</li>
-          <li className ="navBar_li">Skates</li>
+          <li className ="navBar_li">Products</li>
           <li className ="navBar_li">About</li>
           <li className ="navBar_li">Contact</li>
         </ul>
--- src/testimonial.jsx
+++ src/testimonial.jsx
@@ -1,8 +1,19 @@
 function Testimonial(){
   return (
     <div className="article_Sub_Container_One">
-      <section>Hello World</section>
-      <section>Lorem ipsum</section>
+      <section>
+        <h1>Lorem</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Praesentium explicabo labore quisquam quam dicta, sequi autem aspernatur expedita, 
+          excepturi magnam placeat cupiditate cum distinctio quibusdam asperiores impedit dolor 
+          tenetur dolore!</p>
+      </section>
+      <section>
+        <h1>Lorem</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          Vel quaerat est non provident nostrum facilis voluptatum sit ipsam obcaecati! 
+          Soluta reprehenderit eaque iure fugiat facilis quasi sint vel commodi rerum?</p>
+      </section>
     </div>
   )
 }
