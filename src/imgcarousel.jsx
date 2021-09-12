200047093ce24a54c308a510b21f3f738532cd98
Andy Liu <aliu927@gmail.com>
2021-09-12 16:16:10 -0700

    updated carousel bug

--- src/imgcarousel.jsx
+++ src/imgcarousel.jsx
@@ -7,15 +7,15 @@ function ImgCarousel(){
   let imgArray = [imageOne, imageTwo, imageThree];
   let [currentValue, setCurrentValue] = useState(0);
   function btnClick(event){
+    console.log(event)
    //console.log( Number(event.target.attributes[0].value))
-   let changeImg = Number(event.target.attributes[0].value);
+   let changeImg = Number(event.target.attributes[1].value);
    setCurrentValue(currentValue + changeImg);
    if(currentValue >2){
      setCurrentValue(0)
    }else if(currentValue < 0){
      setCurrentValue(2);
    }
-   console.log(currentValue)
   }
 
   return (
